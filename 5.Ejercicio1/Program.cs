97c6bb69a3fb78ea6e780b0f668efcda3441944e
nahuelsolisr <91907898+nahuelsolisr@users.noreply.github.com>
2023-01-06 11:29:15 -0300

    Add files via upload

---
+++ 5.Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Ingresa tu nombre: ");
+string nombre = Console.ReadLine();
+Console.Write("Ingresa tu email: ");
+string email = Console.ReadLine();
+Console.Write("¿Tienes un cupón de descuento? (s/n): ");
+char cupon = Convert.ToChar(Console.ReadLine());
+
+// Precio 
+decimal precioSinDescuento = 100.0m;
+decimal precioConDescuento = 100.0m;
+
+// Si el cliente tiene un cupón de descuento, aplicamos el descuento de 10%
+if (cupon == 's')
+{
+    Console.WriteLine($"Hola {nombre}.");
+    precioConDescuento = precioSinDescuento * 0.9m;
+    Console.WriteLine($"El precio del producto es: {precioConDescuento}.");
+
+}
+else
+{
+    Console.WriteLine($"Hola {nombre}.");
+    Console.WriteLine($"El precio del producto es: {precioSinDescuento}.");
+
+}
+
---
+++ 5.Ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Seleccione un lenguaje de programación");
+Console.WriteLine("a. C# ");
+Console.WriteLine("b. JAVA ");
+Console.WriteLine("c. C++ ");
+
+string opcion = Console.ReadLine();
+
+switch (opcion)
+{
+    case "a":
+        Console.WriteLine("ha saleccionado la opcion de C# ¡Hola, mundo!");
+        break;
+
+    case "b":
+        Console.WriteLine("ha saleccionado la opcion de JAVA");
+        break;
+
+    case "c":
+        Console.WriteLine("ha saleccionado la opcion de C++");
+        break;
+
+    default:
+        Console.WriteLine("opcion no valida");
+        break;
+
+
+}
+
+Console.ReadKey();
